af5131997c742e244046eda58a73694806878ca3
cardoss201 <dmitrij.jakush@gmail.com>
2023-02-10 17:56:35 +0200

    Added redux persist,cart sync with localstorage

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { store } from './redux/store'
+import { store, persitor } from './redux/store'
 
 import App from './App'
 
 import './index.css'
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persitor}>
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,18 +1,42 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { useDispatch } from 'react-redux'
 import filter from './slices/filterSlice'
 import search from './slices/searchSlice'
 import cart from './slices/cartSlice'
 import pizzas from './slices/pizzasSlice'
-import { useDispatch } from 'react-redux'
+
+const rootReducer = combineReducers({ filter, search, cart, pizzas })
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart', 'totalPrice', 'pizzas', 'items'],
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer: {
-    filter,
-    search,
-    cart,
-    pizzas,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 })
+
+export const persitor = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 
